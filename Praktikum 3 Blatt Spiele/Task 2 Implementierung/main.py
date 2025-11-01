0bcac50b57ef4557b64a5ae744066a83ff6122dc
Muhammed Korkmaz <muhammed13@gmx.de>
2025-11-01 12:20:33 +0100

    Part implementation Minimax

--- Praktikum 3 Blatt Spiele/Task 2 Implementierung/main.py
+++ Praktikum 3 Blatt Spiele/Task 2 Implementierung/main.py
@@ -1,7 +1,7 @@
-# Don't forget -> always alternating
-ticTacToeBoard = [1, 1, -1,
-                   1,-1, 0,
-                  -1, 0, 0]
+ticTacToeBoard1 = [0,0,0,0,0,0,0,0,0]
+# Given Tic-Tac-Toe boards for test purposes
+ticTacToeBoard2 = [1,0,0,0,1,0,0,0,0]
+ticTacToeBoard3 = [0,0,0,0,-1,0,0,0,-1]
 
 winCombinations = [
     # diagonals
@@ -27,18 +27,70 @@ def printBoard(ticTacToeBoard):
             print("O", end=" ")
         else:
             print("#", end=" ")
+    print()
 
-def calculateBoard(ticTacToeBoard):
+def utilityFunction(ticTacToeBoard):
     for i in winCombinations:
         sum = 0
         for j in i:
             sum += ticTacToeBoard[j]
         if sum == 3:
-            return "Player 1 wins"
+            return 1
         if sum == -3:
-            return "Player 2 wins"
+            return -1
+    if 0 not in ticTacToeBoard:
+        return 0
     return None
 
-print(calculateBoard(ticTacToeBoard))
-printBoard(ticTacToeBoard)
+def countEmptyPositions(state):
+    emptyPositions = []
+    for j in range(len(state)):
+        if state[j] == 0:
+            emptyPositions.append(j)
+    return emptyPositions
+
+# Max Node
+def Max(state):
+    endstate = utilityFunction(state)
+    if endstate is not None:
+        return endstate
+    bestScore = float("-inf")
+    # Action and subsequent state
+    emptyPositions = countEmptyPositions(state)
+    for i in emptyPositions:
+        state[i] = 1
+        bestScore = max(bestScore, Min(state))
+        state[i] = 0
+    return bestScore
+
+# Min node function
+def Min(state):
+    endstate = utilityFunction(state)
+    if endstate is not None:
+        return endstate
+    bestScore = float("+inf")
+    # Action and subsequent state
+    emptyPositions = countEmptyPositions(state)
+    for i in emptyPositions:
+        state[i] = -1
+        bestScore = min(bestScore, Max(state))
+        state[i] = 0
+    return bestScore
+
+# A node represents a game state
+# We play alternating and Max starts
+# Edges lead to new game states
+# Purpose: Result of a game if both players play perfect
+
+printBoard(ticTacToeBoard1)
+# result 1
+print(Max(ticTacToeBoard1))
+
+printBoard(ticTacToeBoard2)
+# result 2
+print(Max(ticTacToeBoard2))
+
+printBoard(ticTacToeBoard3)
+# result 3
+print(Max(ticTacToeBoard3))
 
